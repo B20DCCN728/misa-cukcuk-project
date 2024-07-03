53e8f53aeddb3bfa2ed6671c983b47be2d995efb
B20DCCN728 <vietjohnny810@gmail.com>
2024-07-03 23:39:31 +0700

    update

--- front-end/js/controlModal.js
+++ front-end/js/controlModal.js
@@ -1,10 +1,13 @@
-/// Open modal
+const firstInputField = document.getElementById('employeeId');
+
+// Open modal
 const btnNew = document.querySelector(".app__content-add");
 btnNew.addEventListener(
     'click',
     () => {
         document.querySelector('.modal').style.display = 'flex';
-        document.getElementById('employeeId').focus();
+        document.querySelector('.modal__body:last-child').style.display = 'none';
+        firstInputField.focus();
     }
 );
 
@@ -16,3 +19,16 @@ btnClose.addEventListener(
         document.querySelector('.modal').style.display = 'none';
     }
 );
+
+// Modal delete product
+const btnDelete = document.querySelectorAll('.app__content-table-cell-action-img');
+for (let i = 0; i < btnDelete.length; i++) {
+    btnDelete[i].addEventListener(
+        'click',
+        () => {
+            console.log('delete');
+            document.querySelector('.modal').style.display = 'flex';
+            document.querySelector('.modal__body:first-child').style.display = 'none';
+        }
+    );
+};
