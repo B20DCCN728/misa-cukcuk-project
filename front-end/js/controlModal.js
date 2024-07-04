040879954f4177bc3cf0cf9678be71bc3d866a32
B20DCCN728 <vietjohnny810@gmail.com>
2024-07-04 14:14:24 +0700

    update sidebar with toggle

--- front-end/js/controlModal.js
+++ front-end/js/controlModal.js
@@ -6,6 +6,7 @@ btnNew.addEventListener(
     'click',
     () => {
         document.querySelector('.modal').style.display = 'flex';
+        document.getElementById('modal__body').style.display = '';
         document.querySelector('.modal__body:last-child').style.display = 'none';
         firstInputField.focus();
     }
@@ -20,15 +21,41 @@ btnClose.addEventListener(
     }
 );
 
+const btnHuy = document.querySelector('button[type="reset"]');
+btnHuy.addEventListener(
+    'click',
+    () => {
+        document.querySelector('.modal').style.display = 'none';
+    }
+);
+
 // Modal delete product
 const btnDelete = document.querySelectorAll('.app__content-table-cell-action-img');
 for (let i = 0; i < btnDelete.length; i++) {
     btnDelete[i].addEventListener(
         'click',
         () => {
-            console.log('delete');
             document.querySelector('.modal').style.display = 'flex';
-            document.querySelector('.modal__body:first-child').style.display = 'none';
+            document.getElementById('modal__body').style.display = 'none';
+            document.querySelector('.modal__body:last-child').style.display = '';
         }
     );
 };
+
+
+// Modal confirm
+const btnCancelDelete = document.querySelector('.modal-buttons .btn-cancel');
+btnCancelDelete.addEventListener(
+    'click',
+    () => {
+        document.querySelector('.modal').style.display = 'none';
+    }
+);
+
+const btnX = document.querySelector('.modal-content .close');
+btnX.addEventListener(
+    'click',
+    () => {
+        document.querySelector('.modal').style.display = 'none';
+    }
+);
